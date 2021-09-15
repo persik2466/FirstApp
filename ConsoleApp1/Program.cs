d6484250501ce0a1625c3296c9dc2fb5defb306f
Irina Lokosova <persik2466@gmail.com>
2021-09-15 11:14:53 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string MyName;
+            MyName = "Irina";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
